f9b07bd183340f6b976b38ac3a8c2482aad0d9b5
Leonardo Bustamante <leobusta@gmail.com>
2022-09-15 08:36:26 -0500

    add django-cors

--- authApp/models/user.py
+++ authApp/models/user.py
@@ -32,7 +32,7 @@ class User(AbstractBaseUser, PermissionsMixin):
   telephone = models.CharField('Telephone', max_length=15,null=True)
 
   def save(self, **kwargs): 
-    some_salt = 'kdqpklhqAAgfd716NKQQ'
+    some_salt = 'mMUj0DrIK6vgtdIYepkIxN'
     self.password = make_password(self.password, some_salt)
     super().save(**kwargs)
   
--- authProject/settings.py
+++ authProject/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'authApp',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,7 +51,16 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+    "https://bank_fe_gr12.herokuapp.com/",
+    "http://127.0.0.1:9000"
+]
+
+#CORS_ALLOW_ALL_ORIGINS = True
 
 REST_FRAMEWORK = {
   'DEFAULT_PERMISSION_CLASSES': (
